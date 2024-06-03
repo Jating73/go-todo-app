ff6d20d9a8d01d5369b5d8a712fab4c2c2e0a099
Jatin Gambhir <jatingambhir22@gmail.com>
2024-06-03 10:08:34 +0530

    Added Custom theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,14 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./chakra/theme.ts";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
